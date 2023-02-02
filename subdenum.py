70f38d308e5bbbe04d2f9abe83ac64511291ed55
eonraider <livewire_voodoo@protonmail.com>
2023-02-02 10:11:20 -0300

    Conform call to new signature of SubdomainScanner

--- subdenum.py
+++ subdenum.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 from src.cli_args import CLIArgumentsParser
 from src.subdomain_scanner import SubdomainScanner
-from src.output import *
+from src.output import FileOutput, ScreenOutput
+
 
 class App:
     def __init__(self):
         self.cli_args = CLIArgumentsParser().parse()
         self.subdomain_scanner = SubdomainScanner(
-                    url=self.cli_args.url,
-                    output_file=self.cli_args.output,
-                    threads=self.cli_args.threads
-                )
+            domains=self.cli_args.domain,
+            output_file=self.cli_args.output,
+            threads=self.cli_args.threads,
+        )
         self.screen_output = ScreenOutput(self.subdomain_scanner)
         if self.cli_args.output:
             self.file_output = FileOutput(self.subdomain_scanner)
