6bc343f1be32670271f68453f8cb84778ef08146
eonraider <livewire_voodoo@protonmail.com>
2023-02-03 08:50:30 -0300

    Move string formatting to parent class

--- subenum/exceptions.py
+++ subenum/exceptions.py
@@ -1,9 +1,9 @@
 class EnumeratorException(Exception):
     def __init__(self, message: str, code: int):
-        super().__init__(message)
+        super().__init__(f"{self.__class__.__name__}: {message}")
         self.code = code
 
 
 class InvalidTargetSpecification(EnumeratorException):
     def __init__(self, message: str, code: int = 1):
-        super().__init__(f"{self.__class__.__name__}: {message}", code)
+        super().__init__(message, code)
