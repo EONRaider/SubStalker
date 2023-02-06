50730d746423333e7ad73a963c5ebb0e56d544b0
eonraider <livewire_voodoo@protonmail.com>
2023-02-06 10:49:36 -0300

    Add base.py

---
+++ subenum/core/parsers/base.py
@@ -0,0 +1,36 @@
+"""
+SubdomainEnumerator: Find subdomains belonging to given target hosts
+using active and passive enumeration methods
+
+Author: EONRaider
+GitHub: https://github.com/EONRaider
+Contact: https://www.twitter.com/eon_raider
+    Copyright (C) 2023 EONRaider @ keybase.io/eonraider
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see
+    <https://github.com/EONRaider/SubdomainEnumerator/blob/master/LICENSE>.
+"""
+
+from abc import ABC, abstractmethod
+
+
+class Parser(ABC):
+    def __init__(self, parser):
+        self.parser = parser
+
+    @property
+    @abstractmethod
+    def enumerators(self) -> set:
+        ...
+
+    @abstractmethod
+    def parse(self, *args, **kwargs):
+        ...
