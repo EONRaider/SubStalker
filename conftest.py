debc2423475d966a6c516590410d17b5ab06ef66
eonraider <livewire_voodoo@protonmail.com>
2023-02-07 07:48:23 -0300

    Add fixture

--- conftest.py
+++ conftest.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 import pytest
 
+from subenum.core.types import EnumResult
+
 
 @pytest.fixture
 def root_dir() -> Path:
@@ -27,6 +29,15 @@ def config_file(root_dir) -> Path:
 @pytest.fixture
 def api_key() -> str:
     return "TOTALLY-LEGIT-API-KEY"
+
+
+@pytest.fixture
+def api_response(target_domain) -> EnumResult:
+    return EnumResult(
+        provider="InstanceOfExternalService",
+        domain=target_domain,
+        subdomains={f"sub{i}.{target_domain}" for i in range(1, 6)},
+    )
 
 
 @pytest.fixture()
