3c22555b241876bdd5b3d262ea2bdaf79a06ff1d
eonraider <livewire_voodoo@protonmail.com>
2023-02-10 19:55:40 -0300

    Add test_enumerator.py

---
+++ tests/unit/test_enumerator.py
@@ -0,0 +1,33 @@
+"""
+SubdomainEnumerator: Find subdomains belonging to given target hosts
+using active and passive enumeration methods
+
+Author: EONRaider
+GitHub: https://github.com/EONRaider
+Contact: https://www.twitter.com/eon_raider
+    Copyright (C) 2023 EONRaider @ keybase.io/eonraider
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see
+    <https://github.com/EONRaider/SubdomainEnumerator/blob/master/LICENSE>.
+"""
+
+
+class TestEnumerator:
+    def test_num_found_domains(self, mock_enumerator, api_response_1, api_response_2):
+        """
+        GIVEN a correctly initialized instance of EnumerationPublisher
+        WHEN this instance has registered found subdomains
+        THEN the "num_found_subdomains" property must return the correct
+            value of known subdomains
+        """
+        mock_enumerator.found_domains[api_response_1.domain] = api_response_1.subdomains
+        mock_enumerator.found_domains[api_response_2.domain] = api_response_2.subdomains
+        assert mock_enumerator.num_found_domains == 10
